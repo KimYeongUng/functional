5c28ecfeaf71e92d0adecf9fbf813e6ebfeb9cfe
kimyeongung <kimyeongung92@gmail.com>
2023-08-27 20:06:53 +0900

    backpressure

---
+++ src/main/java/backpressure/BPEx01.java
@@ -0,0 +1,29 @@
+package backpressure;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class BPEx01 {
+    public static void main(String[] args) {
+        Flux.range(1,5)
+                .doOnRequest(d->log.info("#doOnRequest: {}",d))
+                .subscribe(new BaseSubscriber<>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+
+                    @SneakyThrows
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        Thread.sleep(2000L); // delay
+                        log.info("#hookOnNext: {}", value);
+                        request(1);
+                    }
+                });
+    }
+}
---
+++ src/main/java/backpressure/strategy/ErrorStrategy.java
@@ -0,0 +1,24 @@
+package backpressure.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+@Slf4j
+public class ErrorStrategy {
+    public static void main(String[] args) {
+        Flux.interval(Duration.ofMillis(1L))
+                .onBackpressureError()
+                .doOnNext(d->log.info("#doOnNext: {}",d))
+                .publishOn(Schedulers.parallel())
+                .subscribe(data->{
+                    try {
+                        Thread.sleep(5L);
+                    }catch (InterruptedException ignored){}
+                    log.info("#onNext");
+                }
+                ,err->log.info("#onError"));
+    }
+}
