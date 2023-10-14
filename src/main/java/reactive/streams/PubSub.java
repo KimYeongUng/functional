dc63df86917a2f0f5cee0c166263207bf222b0c6
kimyeongung <kimyeongung92@gmail.com>
2023-10-14 03:25:22 -0700

    Publisher chaining with another threads

--- src/main/java/reactive/streams/PubSub.java
+++ src/main/java/reactive/streams/PubSub.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class PubSub {
@@ -11,6 +15,7 @@ public class PubSub {
         Publisher<Integer> pub = sub-> sub.onSubscribe(new Subscription() {
             @Override
             public void request(long n) {
+                log.debug("#request");
                 sub.onNext(1);
                 sub.onNext(2);
                 sub.onNext(3);
@@ -23,7 +28,52 @@ public class PubSub {
             }
         });
 
-        pub.subscribe(new Subscriber<>() {
+        // processing using other Thread
+        Publisher<Integer> subOnPub = sub->{
+            ExecutorService es = Executors.newSingleThreadExecutor(new CustomizableThreadFactory(){
+                @Override
+                public String getThreadNamePrefix(){
+                    return "subOn-";
+                }
+            });
+
+            es.execute(()->pub.subscribe(sub));
+            es.shutdown();
+        };
+
+        Publisher<Integer> pubOnPub = sub->{
+            subOnPub.subscribe(new Subscriber<>() {
+                ExecutorService es = Executors.newSingleThreadExecutor(new CustomizableThreadFactory(){
+                    @Override
+                    public String getThreadNamePrefix(){
+                        return "pubOn-";
+                    }
+                });
+
+                @Override
+                public void onSubscribe(Subscription s) {
+                    sub.onSubscribe(s);
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    es.execute(()->sub.onNext(integer));
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    es.execute(()->sub.onError(t));
+                }
+
+                @Override
+                public void onComplete() {
+                    es.execute(()->sub.onComplete());
+                    es.shutdown();
+                }
+            });
+        };
+
+        pubOnPub.subscribe(new Subscriber<>() {
             @Override
             public void onSubscribe(Subscription s) {
                 log.debug("OnSubscribe");
